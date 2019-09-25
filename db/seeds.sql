b3aa4341248fc1ee82ea4b28cb082b686b828a02
Tanner Bodrero <tanner.bodrero@gmail.com>
2019-09-25 12:53:23 -0700

    Added functionality to burgers

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('BLT', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Bacon Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name) VALUES ('Egg Burger');
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var express = require("express");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(express.static(process.cwd() + '/public'));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(methodOverride('_method'));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgersController.js");
 
-var routes = require('./controllers/burgers_controller.js');
+app.use(routes);
 
-app.use('/', routes);
-
-app.listen(port);
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
